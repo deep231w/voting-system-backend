3f9d5a9ad357ef0b619fd23f2512a43e1b8b8b68
Deepak kumar maharana <100968930+deep231w@users.noreply.github.com>
2025-10-01 21:49:40 +0530

    voting route added

---
+++ src/routes/vote.js
@@ -0,0 +1,53 @@
+import express from 'express';
+import prisma from '../lib/prisma.js';
+
+const router= express.Router();
+
+router.post('/votesubmit',async(req,res)=>{
+    const userId=req.user.id;
+    const optionId=req.body;
+
+    try{
+        const option=await prisma.pollOption.findUnique({
+            where:{
+                id:optionId
+            },
+            include:{poll:true}
+        })
+
+        if (!option) {
+            return res.status(404).json({ error: "Option not found" });
+        }
+
+        const existingVote = await prisma.vote.findFirst({
+            where: {
+                userId,
+                option: { pollId: option.pollId }
+            }
+        });
+
+        if(existingVote){
+            return res.status(400).send("You have already voted in this poll");
+        }
+
+        const vote = await prisma.vote.create({
+            data: {
+                userId,
+                optionId
+            }
+        });
+
+        res.status(201).json({
+            message:"Vote submitted successfully",
+            vote
+        })
+        
+    }catch(e){
+        console.log("error during vote - ", e);
+
+        res.status(500).json({
+            message:"server error",
+            error:e
+        })
+    }
+})
--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import userRoute from './routes/users.js';
 import pollRoute from './routes/polls.js';
 import { authMiddleware } from './middleware.js';
+import voteRoute from './routes/vote.js';
 
 dotenv.config();
 
@@ -15,6 +16,7 @@ app.use(express.json());
 
 app.use('/users', userRoute);
 app.use('/poll',authMiddleware,pollRoute);
+app.use('/vote',authMiddleware,voteRoute);
 
 
 app.get('/', (req, res) => {
